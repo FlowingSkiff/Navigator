67986367e0e878aee05423c4a4e4b7dd4418aa0c
FlowingSkiff <boats303@gmail.com>
2022-08-13 19:55:38 -0600

    base class for item communcation

---
+++ src/gui/itemcommunication.py
@@ -0,0 +1,12 @@
+
+class GridItem():
+    def __init__(self, id=None, x=None, y=None, color=None):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.color = color
+        pass
+
+
+if __name__ == '__main__':
+    print("itemcommunication has no effect")
