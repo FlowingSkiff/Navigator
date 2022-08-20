351d86d4ba1f3b633634fbdcd67d6c6dacc94984
FlowingSkiff <boats303@gmail.com>
2022-08-20 17:53:33 -0600

    height and width are defined by grid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import gui
 
 if __name__ == '__main__':
-    with gui.TKApplication(width=500, height=400, numx=100, numy=100) as app:
+    with gui.TKApplication(numx=100, numy=100) as app:
         app.run()
